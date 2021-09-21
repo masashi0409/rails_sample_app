b0802a0b64c7e10ae23d879aab9bc51f758c10a1
ShimanoMasashi <masashi.shimano0409@gmail.com>
2021-09-21 04:27:13 +0000

    rails sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e4d7fc5edb2bcc1323eec2c7b125f4cc0a3935bf5a54c265ae845dbb553ba3e3441a5dafb9f18f53f35f4444056987d1dc321ded014a34632691986071569f57'
